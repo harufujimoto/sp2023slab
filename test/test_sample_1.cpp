f88f93149132ddfe2ab0e22011475b3247da1fe5
Haruki Fujimoto <harufujimoto.311@gmail.com>
2023-11-02 12:16:08 +0900

    modify sample test

---
+++ test/test_sample_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+#include "./../src/practice.cpp"
+
+int main() {
+  const long P = 998244353;
+  const long a = 2023;
+
+  std::vector<long> n = {0, 1, 10, 100, 1000};
+  std::vector<long> z = {
+      1, 2023, 329969289, 340092910, 800061172,
+  };
+
+  int cnt = 0;
+  for (int i = 0; i < (int)n.size(); i++) {
+    std::cout << "Case #" << i << ": ";
+    long res;
+    pow(res, a, n[i], P);
+    if (res == z[i]) {
+      std::cout << "OK" << std::endl;
+      cnt++;
+    } else {
+      std::cout << "ERROR" << std::endl;
+      std::cout << "actual : " << res << std::endl;
+      std::cout << "expected : " << z[i] << std::endl;
+    }
+  }
+
+  std::cout << cnt << "/" << (int)n.size() << " cases passed" << std::endl;
+  if (cnt != (int)n.size()) {
+    exit(-1);
+  }
+}
